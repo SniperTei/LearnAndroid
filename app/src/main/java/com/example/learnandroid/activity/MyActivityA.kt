d886d2c2e16aebeb6dbd01d7689535f4fb1eb566
Sniper <sniper_zheng@163.com>
2023-10-31 22:20:22 +0800

    fragment

--- app/src/main/java/com/example/learnandroid/activity/MyActivityA.kt
+++ app/src/main/java/com/example/learnandroid/activity/MyActivityA.kt
@@ -22,24 +22,24 @@ class MyActivityA : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_my_activity)
 
-        // for循环30次
-        data = (0..30).map {
-            MyABean("name", it)
-        }
-        Log.i("Sniper", "aaa : $data")
-
-        val myRecyclerView = findViewById<RecyclerView>(R.id.my_rv)
-        val myAAdapter = MyAAdapter(this, data)
-////        val layoutManager = GridLayoutManager(this, 3)
-        val layoutManager = LinearLayoutManager(this)
-        myRecyclerView.layoutManager = layoutManager
+//        // for循环30次
+//        data = (0..30).map {
+//            MyABean("name", it)
+//        }
+//        Log.i("Sniper", "aaa : $data")
 //
-        myRecyclerView.adapter = myAAdapter
-        myAAdapter.setOnItemClickListener(object : MyAAdapter.OnItemClickListener {
-            override fun onItemClick(view: View, position: Int) {
-                Log.i("Sniper", "item clicked: $position")
-            }
-        })
+//        val myRecyclerView = findViewById<RecyclerView>(R.id.my_rv)
+//        val myAAdapter = MyAAdapter(this, data)
+//////        val layoutManager = GridLayoutManager(this, 3)
+//        val layoutManager = LinearLayoutManager(this)
+//        myRecyclerView.layoutManager = layoutManager
+////
+//        myRecyclerView.adapter = myAAdapter
+//        myAAdapter.setOnItemClickListener(object : MyAAdapter.OnItemClickListener {
+//            override fun onItemClick(view: View, position: Int) {
+//                Log.i("Sniper", "item clicked: $position")
+//            }
+//        })
 
         // 另一种方式 创建一个textview
 //        val layout = LinearLayout(this)
@@ -50,6 +50,9 @@ class MyActivityA : AppCompatActivity() {
 //        textView.text = "This is a text"
 //        layout.addView(textView)
 //        setContentView(layout)
+
+        // fragment
+
     }
 
 
---
+++ app/src/main/java/com/example/learnandroid/fragment/BlankFragment1.kt
@@ -0,0 +1,46 @@
+package com.example.learnandroid.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import com.example.learnandroid.R
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [BlankFragment1.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class BlankFragment1 : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val root = inflater.inflate(R.layout.fragment_blank1, container, false)
+        val textView = root.findViewById<TextView>(R.id.hello_fragment_text)
+        val okBtn = root.findViewById<Button>(R.id.hello_fragment_btn)
+
+        okBtn.setOnClickListener {
+            textView.text = "Okay btn clicked and text changed"
+        }
+
+        return root
+    }
+}
