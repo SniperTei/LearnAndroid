f0af8fba3594baa754fb778fdaf5313d840de110
Sniper <sniper_zheng@163.com>
2024-01-28 16:53:45 +0800

    login

---
+++ my_module/src/main/java/com/example/my_module/ui/fragment/LoginFragment.kt
@@ -0,0 +1,50 @@
+package com.example.my_module.ui.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.common_library.app.base.fragment.BaseVmFragment
+import com.example.common_library.log.LogUtil
+import com.example.my_module.R
+import com.example.my_module.viewmodel.LoginViewModel
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [LoginFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class LoginFragment : BaseVmFragment<LoginViewModel>() {
+
+    private val TAG = "LoginFragment"
+    override fun layoutId(): Int {
+        return R.layout.fragment_login
+    }
+
+    override fun initView(savedInstanceState: Bundle?) {
+        LogUtil.debugInfo(TAG, "initView")
+    }
+
+    override fun lazyLoadData() {
+        LogUtil.debugInfo(TAG, "lazyLoadData")
+    }
+
+    override fun createObserver() {
+        LogUtil.debugInfo(TAG, "createObserver")
+    }
+
+    override fun startLoading(message: String) {
+
+    }
+
+    override fun stopLoading() {
+
+    }
+
+}
--- my_module/src/main/java/com/example/my_module/ui/fragment/MyFragment.kt
+++ my_module/src/main/java/com/example/my_module/ui/fragment/MyFragment.kt
@@ -1,7 +1,11 @@
 package com.example.my_module.ui.fragment
 
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import androidx.core.widget.NestedScrollView
 import androidx.fragment.app.viewModels
+import androidx.transition.Visibility
 import com.example.common_library.app.base.fragment.BaseVmFragment
 import com.example.common_library.log.LogUtil
 import com.example.my_module.R
@@ -19,6 +23,20 @@ class MyFragment : BaseVmFragment<MyViewModel>() {
 
     override fun initView(savedInstanceState: Bundle?) {
         LogUtil.debugInfo(TAG, "My fragment")
+        val root = view ?: return
+        val tv_name = root.findViewById<TextView>(R.id.me_name)
+        // 获取 NavController
+       val navController =
+//        val nsv_my_info = root.findViewById<NestedScrollView>(R.id.nsv_my_info)
+        tv_name.setOnClickListener {
+
+            // 展示登录fragment
+            val loginFragment = LoginFragment()
+            val transaction = parentFragmentManager.beginTransaction()
+            transaction.replace(R.id.fragment_container_view_tag, loginFragment)
+            transaction.addToBackStack(null)
+            transaction.commit()
+        }
     }
 
     override fun lazyLoadData() {
